4e88e80ce058e941463221d6aae96694b59bc458
Ralfs Fadejevs <ralphfdesigns@gmail.com>
2024-03-12 13:01:04 +0100

    Fixed the bug

--- pong_game.py
+++ pong_game.py
@@ -9,7 +9,7 @@ playerBscore=0
 #create a window and declare a variable called window and call the screen()
 window=t.Screen()
 window.title("The Pong Game")
-window.bgcolor("yellow")
+window.bgcolor("green")
 window.setup(width=800,height=600)
 window.tracer(0)
   
